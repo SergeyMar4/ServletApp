b3829047fc09f0925ca6b97b5d074ab3e7440dd2
SergeyMar4 <144382673+SergeyMar4@users.noreply.github.com>
2024-02-02 17:21:47 +0300

    Реализация класса MyFirstServlet

--- src/main/java/com/sergeymar4/servletapp/rest/MyFirstServlet.java
+++ src/main/java/com/sergeymar4/servletapp/rest/MyFirstServlet.java
@@ -1,2 +1,21 @@
-package com.sergeymar4.servletapp.rest;public class MyFirstServlet {
+package com.sergeymar4.servletapp.rest;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<body>");
+        pw.println("<h3>Прикладная информатика</h3>");
+        pw.println("</body>");
+        pw.println("</html>");
+    }
 }
